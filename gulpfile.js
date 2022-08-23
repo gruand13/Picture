508513c7e0d3b5bebc98d6b2a452c1d9438e190b
Andrey Hrusheuski <gruand13@gmail.com>
2022-08-23 19:53:41 +0300

    made forms send to server( but without testing( I have not MAMP yet)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// можно написать адрес на локальном сервере
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
